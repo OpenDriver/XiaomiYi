87e4fdc5b64858bc4488cdeed1bebc2278f476db
luisgiii <loiis.x14@gmail.com>
2016-02-25 23:30:25 -0600

    Initial commit

---
+++ xiaomiCV.py
@@ -0,0 +1,11 @@
+'''
+Only for Mac OS X
+How-to get Xiaomi Yi Camera IP address to send commands to it through sockets
+ airport -s | grep "XDJ"
+ - parse output with python to save only SSID.
+ networksetup -setairportnetwork $INTERFACE $SSID $PASSWORD
+ Xiaomi Yi password: 1234567890
+ ifconfig | grep "192.168.42"
+ - parse output with python to only get Xiaomi IP address.
+ - do magic!
+'''
